03019497d0573b248de563076625385a7e11b3e9
AbhishekUS5 <abhishekshet12@gmail.com>
2026-02-15 13:47:44 +0530

    Config beans

--- 1. Spring Core/8. Config Bean/src/main/java/com/Sam/SpringDemo/common/CricketCoach.java
+++ 1. Spring Core/8. Config Bean/src/main/java/com/Sam/SpringDemo/common/CricketCoach.java
@@ -17,15 +17,4 @@ public class CricketCoach implements Coach {
     public void doMyStartUpStuff(){
         System.out.println("In doMyStartupStuff():" + getClass().getSimpleName());
     }
-
-    // Define our destroy method
-    @PreDestroy
-    public void doMyCleanUpStuff(){
-        System.out.println("In doMyCleanUpStuff():" + getClass().getSimpleName());
-    }
-
-    @Override
-    public String getDailyWorkOut(){
-        return "Practice fast bowling for 15 minutes :-]";
-    }
 }
