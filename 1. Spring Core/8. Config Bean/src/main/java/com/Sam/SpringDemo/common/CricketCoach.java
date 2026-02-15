92e9e1cc8a94925ddaa45791ab6611fc149fb33b
AbhishekUS5 <abhishekshet12@gmail.com>
2026-02-15 13:48:49 +0530

    Config beans

--- 1. Spring Core/8. Config Bean/src/main/java/com/Sam/SpringDemo/common/CricketCoach.java
+++ 1. Spring Core/8. Config Bean/src/main/java/com/Sam/SpringDemo/common/CricketCoach.java
@@ -12,9 +12,8 @@ public class CricketCoach implements Coach {
         System.out.println("In constructor: "+ getClass().getSimpleName() );
     }
 
-    // Define our init method
-    @PostConstruct
-    public void doMyStartUpStuff(){
-        System.out.println("In doMyStartupStuff():" + getClass().getSimpleName());
+    @Override
+    public String getDailyWorkOut(){
+        return "Practice fast bowling for 15 minutes :-]";
     }
 }
